140d6b5de3dbce80c858ccbd5195d84761c891cc
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-04-22 18:00:52 +0600

    designed pagination button

--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -1,24 +1,39 @@
 import { useState, useEffect } from "react";
 import apiClient from "/src/components/services/api_client.js";
 import ProductList from "/src/components/Shop/ProductList.jsx";
+import Pagination from "/src/components/Shop/Pagination.jsx";
 
 const ShopPage = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = () => {
     setLoading(true);
     apiClient
       .get("/products/")
-      .then((res) => setProducts(res.data.results))
+      .then((res) => {
+        setProducts(res.data.results);
+        setTotalPages(Math.ceil(res.data.count / res.data.results.length));
+      })
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
-  }, []);
+  };
 
   return (
     <div>
       <ProductList products={products} loading={loading} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        handleCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };

5ef43f0e95652169b334fdde0c1fbbcac2f4c465
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-04-23 10:04:17 +0600

    price range changed dynamically

---
+++ src/components/Shop/FilterSection.jsx
@@ -0,0 +1,111 @@
+const FilterSection = ({ priceRange, handlePriceChange }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+      {/*Price Range*/}
+      <div className="bg-white p-4 rounded-lg shadow">
+        <label
+          htmlFor=""
+          className="block text-sm font-medium text-gray-700 mb-2"
+        >
+          Price Range
+        </label>
+
+        {/*minimum range*/}
+        <div className="flex items-center space-x-4">
+          <input
+            type="number"
+            min="0"
+            step="10"
+            max={priceRange[1]}
+            value={priceRange[0]}
+            onChange={(e) => handlePriceChange(0, Number(e.target.value))}
+            className="w-20 p-2 border rounded-md"
+          />
+          <input
+            type="range"
+            min="0"
+            max={priceRange[1]}
+            step="10"
+            value={priceRange[0]}
+            onChange={(e) => handlePriceChange(0, Number(e.target.value))}
+            className="w-full"
+          />
+        </div>
+
+        {/*maximum range*/}
+        <div className="flex items-center space-x-4 mt-2">
+          <input
+            type="number"
+            min={priceRange[0]}
+            max="1000"
+            step="10"
+            value={priceRange[1]}
+            onChange={(e) => handlePriceChange(1, Number(e.target.value))}
+            className="w-20 p-2 border rounded-md"
+          />
+          <input
+            type="range"
+            min={priceRange[0]}
+            max="1000"
+            step="10"
+            value={priceRange[1]}
+            onChange={(e) => handlePriceChange(1, Number(e.target.value))}
+            className="w-full"
+          />
+        </div>
+
+        <div className="flex justify-between text-sm text-gray-600 mt-2">
+          <span>${priceRange[0]}</span>
+          <span>${priceRange[1]}</span>
+        </div>
+      </div>
+
+      {/*Category Filter*/}
+      <div className="bg-white p-4 border rounded-md">
+        <label
+          htmlFor=""
+          className="block text-sm font-medium text-gray-700 mb-2"
+        >
+          Category
+        </label>
+        <select name="" id="" className="w-full p-2 border rounded-md">
+          <option value="">All Categories</option>
+          <option value="1">Book</option>
+          <option value="2">Fashion</option>
+        </select>
+      </div>
+
+      {/*Searching*/}
+      <div className="bg-white p-4 rounded-lg shadow">
+        <label
+          htmlFor=""
+          className="block text-sm font-medium text-gray-700 mb-2"
+        >
+          Search
+        </label>
+        <input
+          type="text"
+          className="w-full p-2 border rounded-md"
+          placeholer="Search books..."
+        />
+      </div>
+
+      {/*Sorting*/}
+      <div className="bg-white p-4 rounded-lg shadow">
+        <label
+          htmlFor=""
+          className="block text-sm font-medium text-gray-700 mb-2"
+        >
+          Sort By Price
+        </label>
+        <select name="" id="" className="w-full p-2 border rounded-md">
+          <option value="">Default</option>
+          <option value="asc">Price: Low to High</option>
+          <option value="desc">Price: High to Low</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default FilterSection;
--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -2,10 +2,21 @@ import { useState } from "react";
 import ProductList from "/src/components/Shop/ProductList.jsx";
 import Pagination from "/src/components/Shop/Pagination.jsx";
 import useFetchProducts from "/src/hooks/useFetchProducts.js";
+import FilterSection from "/src/components/Shop/FilterSection.jsx";
 
 const ShopPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
   const { products, loading, totalPages } = useFetchProducts(currentPage);
+
+  const handlePriceChange = (index, value) => {
+    setPriceRange((prev) => {
+      const newRange = [...prev];
+      newRange[index] = value;
+      return newRange;
+    });
+    setCurrentPage(1);
+  };
 
   // const fetchProducts = () => {
   //   setLoading(true);
@@ -20,7 +31,12 @@ const ShopPage = () => {
   // };
 
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8">Shop Our Products</h1>
+      <FilterSection
+        priceRange={priceRange}
+        handlePriceChange={handlePriceChange}
+      />
       <ProductList products={products} loading={loading} />
       <Pagination
         totalPages={totalPages}
